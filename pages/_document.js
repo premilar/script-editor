2a7c1c78289eba48f1e32c86afb89071219c33d5
Premila Rowles <premila@mit.edu>
2023-07-11 19:27:00 -0700

    revert

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import filmLogo from '../assets/film.png';
+import buildspaceLogo from '../assets/buildspace-logo.png';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="AI Script Editor" key="title"/>
+        <meta property="og:title" content="GPT-3 Writer" key="title"/>
         <meta property="og:description" content="build with buildspace" key="description"/>
         <meta
           property="og:image"
-          content="https://github.com/premilar/script-editor/blob/main/assets/film.png"
+          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
